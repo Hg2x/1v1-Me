69ac6516c93591d9ce1deef2be196dfbd3e15c94
Hg2x <92201112+Hg2x@users.noreply.github.com>
2023-03-19 10:43:46 +0800

    Add health and attack to UnitDataBase

--- Assets/Scripts/Units/UnitDataBase.cs
+++ Assets/Scripts/Units/UnitDataBase.cs
@@ -4,6 +4,8 @@ using static ICKT.Editor.EditorLibrary;
 public abstract class UnitDataBase : ScriptableObject
 {
 	[Header("Base Unit Related")]
+	[Min(0)] public float Health = 10;
+	[Min(0)] public float Attack = 1;
 	[ReadOnlyField] public bool IsFacingRight;
 	[Min(0)] public float BaseMoveSpeed;
 	[ReadOnlyField] public bool IsAttacking = false;

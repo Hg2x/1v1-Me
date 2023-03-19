0d582e727a8d6a3d1863bfe22e59021db706ea37
Hg2x <92201112+Hg2x@users.noreply.github.com>
2023-03-19 09:42:21 +0800

    Rename folder, some small adjustments

--- Assets/Scripts/Units/StoneMechaGolemUnit.cs
+++ Assets/Scripts/Units/StoneMechaGolemUnit.cs
@@ -6,6 +6,14 @@ public class StoneMechaGolemUnit : UnitBase
 {
 	private StoneMechaGolemUnitData _Data;
 
+	private const string IDLE = "Idle";
+	private const string ATTACK = "Attack";
+	private const string SHOOT_ARM = "ShootArm";
+	private const string SHOOT_LASER = "ShootLaser";
+
+	private float _ElapsedTime = 0;
+	private float _TotalTime = 0;
+
 	protected override void Awake()
 	{
 		base.Awake();
@@ -20,6 +28,25 @@ public class StoneMechaGolemUnit : UnitBase
 
 	private void Start()
 	{
-		//ChangeAnimationState(IDLE);
+		ChangeAnimationState(IDLE);
+	}
+
+	private void Update()
+	{
+		_ElapsedTime += Time.deltaTime;
+		_TotalTime += Time.deltaTime;
+
+		if (_ElapsedTime >= 1)
+		{
+			_ElapsedTime -= 1;
+			if (_CurrentAnimation == IDLE)
+			{
+				ChangeAnimationState(ATTACK);
+			}
+			else
+			{
+				ChangeAnimationState(IDLE);
+			}
+		}
 	}
 }

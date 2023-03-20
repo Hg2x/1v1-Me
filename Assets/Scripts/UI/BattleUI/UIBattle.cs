21312b394b6149abe46bbf9a64d37d1128895c74
Hg2x <92201112+Hg2x@users.noreply.github.com>
2023-03-20 14:22:38 +0800

    Add enemy unit info UI in BattleUI

--- Assets/Scripts/UI/BattleUI/UIBattle.cs
+++ Assets/Scripts/UI/BattleUI/UIBattle.cs
@@ -7,14 +7,17 @@ public class UIBattle : UIBase
 {
     [SerializeField] private UIBattleTimer _TimerUI;
 	[SerializeField] private UIBattleUnitInfo _PlayerUnitInfoUI;
+	[SerializeField] private UIBattleUnitInfo _EnemyUnitInfoUI;
 
 	// TODO: use a better way to get unit infos
 	[SerializeField] private UnitDataBase _PlayerData;
+	[SerializeField] private UnitDataBase _EnemyData;
 
 	private void Start()
 	{
 		ServiceLocator.Get<LevelManager>().OnSecondPassed += UIBattle_OnSecondPassed;
 		_PlayerUnitInfoUI.Initialize(_PlayerData);
+		_EnemyUnitInfoUI.Initialize(_EnemyData);
 	}
 
 	private void UIBattle_OnSecondPassed(float totalTime)
